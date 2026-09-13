6aa8691e190cfee9899f2e0258ae762206d07251
1337farm <danbrianwhite+1337farm@gmail.com>
2026-09-13 13:35:32 -0400

    fix: use published AICore artifact 0.0.1-exp02 (#1)
    * fix: use published AICore artifact 0.0.1-exp02, suppress compileSdk 35 warning
    
    * fix: bump Kotlin 2.1.0 + AGP 8.7.3 + Gradle 8.9 for AICore metadata
    
    * fix: use public TimeoutCancellationException(message, owner) ctor
    
    * fix: drop manual TimeoutCancellationException construction (internal in coroutines 1.9)
    
    * fix: honor $1 group ref in secret-mask replacement (template mode)
    
    ---------
    
    Co-authored-by: forgerig-ci <ci@1337farm.local>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file shared by all modules.
 plugins {
-    id("com.android.library") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22" apply false
+    id("com.android.library") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0" apply false
 }
